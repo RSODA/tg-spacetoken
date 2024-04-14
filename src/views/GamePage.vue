77b29df047057c75f5170154fe032f582968950a
RSODA <135031771+RSODA@users.noreply.github.com>
2024-04-14 19:34:45 +0300

    Update GamePage.vue

--- src/views/GamePage.vue
+++ src/views/GamePage.vue
@@ -48,7 +48,7 @@ export default {
 
 <template>
         <div class="bg-stone-950 w-full h-[1080px] mx-auto">
-            <p class="center text-2xl mx-auto text-center relative py-3">Hello, {{ this.users.user.name }}</p>
+            <p class="center text-2xl mx-auto text-center relative py-[50%]">Hello, {{ this.users.user.name }}</p>
             <form @submit="tokenplus">
             <p class="center text-center text-5xl py-[10vh]">{{ this.users.user.sstoken }}</p>
             <button class="btntwoo mx-auto animate-spin" type="submit">
