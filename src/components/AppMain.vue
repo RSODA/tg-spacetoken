dd0002ce1fedc752acdbed56d9783e99224cec71
RSODA <135031771+RSODA@users.noreply.github.com>
2024-04-17 22:10:38 +0300

    Update AppMain.vue

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -39,6 +39,7 @@ export default {
         <div class="bg-stone-950 w-full h-[1080px] mx-auto">
             <p class="center text-2xl mx-auto text-center relative py-3">Hello, {{ this.usertg.username }}</p>
             <form @submit="tokenplus">
+              <p class="center text-center text-5xl py-[10vh]">{{this.usertg}}</p>
             <p class="center text-center text-5xl py-[10vh]">{{this.usertg._id}}</p>
             <button class="btntwoo mx-auto animate-spin" type="submit">
                 <img src="../assets/sscoins.png" class="">
