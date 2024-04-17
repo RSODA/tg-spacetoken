efc7cdf6e83df22006d736d0c55848d8715627a5
RSODA <135031771+RSODA@users.noreply.github.com>
2024-04-17 21:22:24 +0300

    Update GamePage.vue

--- src/views/GamePage.vue
+++ src/views/GamePage.vue
@@ -59,7 +59,7 @@ export default {
         <div class="bg-stone-950 w-full h-[1080px] mx-auto">
             <p class="center text-2xl mx-auto text-center relative py-3">Hello, {{ this.users.user.name }}</p>
             <form @submit="tokenplus">
-            <p class="center text-center text-5xl py-[10vh]">{{ this.users.user.sstoken }} {{ this.usertg.username }}</p>
+            <p class="center text-center text-5xl py-[10vh]">{{ this.users.user.sstoken }} {{ window.Telegram.WebApp.initDataUnsafe?.user?.username }}</p>
             <button class="btntwoo mx-auto animate-spin" type="submit">
                 <img src="../assets/sscoins.png" class="">
             </button>
